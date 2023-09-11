f6447e42808d1d2513419c2fab8913f73bb51907
nathan-j-lee <127238376+nathan-j-lee@users.noreply.github.com>
2023-09-11 16:58:11 -0700

    Learning Gamepad APi

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 window.addEventListener("gamepadconnected", (e) => {
     console.log(
-    "Gamepad connected at index %d: %s. %d buttons, %d axes.",
-    e.gamepad.index,
-    e.gamepad.id,
-    e.gamepad.buttons.length,
-    e.gamepad.axes.length
+      "Gamepad connected at index %d: %s. %d buttons, %d axes.",
+      e.gamepad.index,
+      e.gamepad.id,
+      e.gamepad.buttons.length,
+      e.gamepad.axes.length,
     );
 });
-
-const gp = navigator.getGamepads()[0];
-console.table(gp.buttons);

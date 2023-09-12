8752076543ac2883da19113b9c7451346df410c1
nathan-j-lee <127238376+nathan-j-lee@users.noreply.github.com>
2023-09-12 16:15:43 -0700

    Display some buttons when buttons pressed

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let left = false;
+let right = false;
+let up = false;
+let down = false;
+
+let lk = false;
+let lp = false;
+let mk = false;
+let mp = false;
+let hk = false;
+let hp = false;
+
 window.addEventListener("gamepadconnected", (e) => {
     console.log(
       "Gamepad connected at index %d: %s. %d buttons, %d axes.",
@@ -6,4 +18,20 @@ window.addEventListener("gamepadconnected", (e) => {
       e.gamepad.buttons.length,
       e.gamepad.axes.length,
     );
+    gameLoop();
 });
+
+function gamepadInput() {
+  const gamepad = navigator.getGamepads()[0];
+  const buttons = gamepad.buttons;
+  lk = buttons[0].pressed;
+  mk = buttons[1].pressed;
+  lp = buttons[2].pressed;
+  mp = buttons[3].pressed;
+  if (lk || lp || mp || mk) console.log(buttons);
+}
+
+function gameLoop() {
+  gamepadInput();
+  requestAnimationFrame(gameLoop);
+}
